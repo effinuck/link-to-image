8cb1caae13dc15bdabb1ed345aed5498df94f917
effinuck <davidcunniffe@gmail.com>
2026-05-22 12:56:15 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -112,8 +112,17 @@ async function handleMetadata(req, res, target) {
     const response = await fetch(target, {
       redirect: "follow",
       headers: {
-        "accept": "text/html,application/xhtml+xml",
-        "user-agent": "Mozilla/5.0 link-preview-card-agent"
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+        "accept-language": "en-IE,en-GB;q=0.9,en;q=0.8",
+        "accept-encoding": "gzip, deflate, br",
+        "cache-control": "no-cache",
+        "pragma": "no-cache",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "none",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
       }
     });
 
